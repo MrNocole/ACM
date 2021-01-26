34aadab2ec99ecbc65a149adacb9091baca47985
ZhangTao <53431527+MrNocole@users.noreply.github.com>
2021-01-26 14:07:13 +0800

    Create 排列组合.cpp

---
+++ 排列组合.cpp
@@ -0,0 +1,24 @@
+组合数（含逆元）
+ll qp(ll b ,ll p){
+	ll res = 1;
+	while (p){
+		if (p%2)res=res*b%mod;
+		p /= 2;
+		b = (b*b)%mod;
+	}
+	return res;
+}
+
+ll ny(long long x){
+	return qp(x,mod-2);
+}
+
+ll C(ll n , ll k){
+	ll res = 1;
+	for (ll i = 1 ; i <= k ; i ++){
+		res = res*(n-i+1)%mod*ny(i)%mod;
+	}
+	return res%mod;
+}
+
+C是入口，n是全集大小，k是要选出的数。
