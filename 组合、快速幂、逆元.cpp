b3f6102df66def901cdcb8192d4a8d465571b0e0
ZhangTao <53431527+MrNocole@users.noreply.github.com>
2021-02-18 15:55:53 +0800

    Update 组合、快速幂、逆元.cpp

--- 组合、快速幂、逆元.cpp
+++ 组合、快速幂、逆元.cpp
@@ -20,5 +20,12 @@ ll C(ll n , ll k){
 	}
 	return res%mod;
 }
-
 //C是入口，n是全集大小，k是要选出的数。
+
+用一个二维数组C[N][N]来表示组合。
+for (int i = 0; i < n; ++i) {
+  for (int j = 0; j <= i; ++j) {
+    if (!j) C[i][j] = 1;
+    else C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]);
+  }
+}
