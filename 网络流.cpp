3b288002c27d1e36e7ff46cb443d47218651a187
ZhangTao <53431527+MrNocole@users.noreply.github.com>
2021-04-05 19:12:56 +0800

    Update 网络流.cpp

--- 网络流.cpp
+++ 网络流.cpp
@@ -37,3 +37,70 @@ int max_flow (int s , int t){
         flow += f;
     }
 }
+
+
+//Dinic
+const ll INF = 1e9+7;
+const int N = 11000 , E = 210000;
+int n , m , s , t;
+ll cnt = 1 , first[N] , nxt[E] , to[E] , val[E];
+
+inline void addEdge(int u , int v , ll w){
+    to[++cnt] = v;
+    val[cnt] = w;
+    nxt[cnt] = first[u];
+    first[u] = cnt;
+}
+int dep[N] , q[N] , l ,r;
+bool bfs(){
+    memset(dep,0,sizeof dep);
+    q[l=r=1] = s;
+    dep[s] = 1;
+    while (l <= r){
+        int u = q[l++];
+        for (int p = first[u] ; p ; p = nxt[p]){
+            int v = to[p];
+            if (val[p] and !dep[v]){
+                dep[v] = dep[u] + 1;
+                q[++r] = v;
+            }
+        }
+    }
+    return dep[t];
+}
+ll dfs(int u , ll in){
+    if (u==t)return in;
+    ll out = 0;
+    for (int p = first[u]  ; p and in ; p = nxt[p]) {
+        int v = to[p];
+        if (val[p] and dep[v] == dep[u] + 1){
+            ll res = dfs(v,min(val[p],in));
+            val[p] -= res;
+            val[p^1] += res;
+            in -= res;
+            out += res;
+        }
+    }
+    if (out == 0){
+        dep[u] = 0;
+    }
+    return out;
+}
+
+ll max_flow(){
+    ll ret = 0;
+    while (bfs())ret+=dfs(s,std::numeric_limits<ll>::max());
+    return ret;
+}
+//建边 要建双向边
+int main(){
+    scanf("%d%d%d%d",&n,&m,&s,&t);
+    for (int i = 1 ; i <= m ; i ++){
+        int u , v;
+        ll w;
+        scanf("%d%d%lld",&u,&v,&w);
+        addEdge(u,v,w);
+        addEdge(v,u,0);
+    }
+    printf("%lld\n",max_flow());
+}
