ea09b77c51ddf06181e4bde870aaa89308e7556c
MrNocole <1926276913@qq.com>
2021-11-09 08:26:52 +0800

    test

--- 组合、快速幂、逆元.cpp
+++ 组合、快速幂、逆元.cpp
@@ -1,8 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-09 07:55:41
+ * @LastEditTime: 2021-11-09 08:24:19
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /ACM/组合、快速幂、逆元.cpp
+ */
 //组合数（含逆元）
 ll qp(ll b ,ll p){
 	ll res = 1;
 	while (p){
-		if (p&1)res=res*b%mod;
+		if (p&1)res=(res*b)%mod;
 		p >>= 1;
 		b = (b*b)%mod;
 	}
